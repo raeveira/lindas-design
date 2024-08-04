cf8318f0c15918572a41434cdb4a3c8676b3db69
raeveira <rae@raeveira.nl>
2024-08-04 21:17:04 +0200

    created images

--- components/review.tsx
+++ components/review.tsx
@@ -5,14 +5,13 @@ import Image from 'next/image';
 interface CardProps {
     name: string;
     content: string;
-    image: string;
     date: string;
     stars: number;
-    locale: 'nl' | 'enUS';
+    images?: { src: string }[];
 }
 
 
-const ReviewCard: React.FC<CardProps> = ({ name, content, image, stars }) => {
+const ReviewCard: React.FC<CardProps> = ({ name, content, stars, images }) => {
     const fullStars = Math.floor(stars);
     const hasHalfStar = stars % 1 !== 0;
     const starFill = stars - fullStars;
@@ -20,11 +19,17 @@ const ReviewCard: React.FC<CardProps> = ({ name, content, image, stars }) => {
     return (
         <div className="container border-solid border-2 w-full m-2 p-3 flex flex-col space-y-3">
             <div className="mr-3 flex space-x-2">
-                <Image src={image} alt={`profile photo of ${name}`} height={25} width={25} className='rounded-full h-[25px] w-[25px] object-cover' />
                 <h3 className='font-semibold'>{name}</h3>
             </div>
             <div className="flex-1">
                 <p>{content}</p>
+                {images && (
+                    <div className="flex flex-wrap gap-2 mt-2">
+                        {images.map((image, index) => (
+                            <Image key={index} src={image.src} alt={`review-${index}`} width={250} height={700} className="w-60 h-auto object-cover rounded-md hover:scale-150 transition-all duration-500" />
+                        ))}
+                    </div>
+                )}
             </div>
             <div className='text-xs flex'>
                 <div className='flex w-full justify-end'>
